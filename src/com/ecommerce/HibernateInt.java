684327b020f80aaf77ceaf33d2eabd8c56587bbf
Leina <Leina@MSI>
2021-01-21 11:10:43 +0300

    Configuration Hibernate and list product from DB

--- src/com/ecommerce/HibernateInt.java
+++ src/com/ecommerce/HibernateInt.java
@@ -2,6 +2,9 @@ package com.ecommerce;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 
@@ -31,15 +35,33 @@ public class HibernateInt extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		PrintWriter out=response.getWriter();
-		out.print("<html><body>");
-		SessionFactory sessionFactory=HibernetUtil.getSessionFactory();
-		sessionFactory.openSession();
-		out.print("open session");
-		out.print("is session open?"+sessionFactory.isOpen());
-		out.print("</body></html>");
-	}
+		try {
+            SessionFactory factory = HibernetUtil.getSessionFactory();
+            
+            Session session = factory.openSession();
+            // using HQL
+            List<Product> list = session.createQuery("from Product").list();
+            
+            session.close();
+            
+             PrintWriter out = response.getWriter();
+             out.println("<html><body>");
+             out.println("<b>Product Listing</b><br>");
+             for(Product p: list) {
+                     out.println("ID: " + String.valueOf(p.getID()) + ", Name: " + p.getName() +
+                                     ", Price: " + String.valueOf(p.getPrice()) + ", Date Added: " + p.getDateAdded().toString() + "<br>");
+             }
+             
+         out.println("</body></html>");
+         
+         
+     } catch (Exception ex) {
+             throw ex;
+     }
+        
+}
+
+       
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
--- src/com/ecommerce/Product.java
+++ src/com/ecommerce/Product.java
@@ -1,45 +1,35 @@
 package com.ecommerce;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
 public class Product {// pojo class , bean
 
-	private int pid;
-	private String pname;
-	private double price;
-	private Date Dop;
-	public int getPid() {
-		return pid;
-	}
-	public void setPid(int pid) {
-		this.pid = pid;
-	}
-	@Override
-	public String toString() {
-		return "Product [pid=" + pid + ", pname=" + pname + ", price=" + price + ", Dop=" + Dop + "]";
-	}
-	public String getPname() {
-		return pname;
-	}
-	public void setPname(String pname) {
-		this.pname = pname;
-	}
-	public double getPrice() {
-		return price;
-	}
-	public void setPrice(double price) {
-		this.price = price;
-	}
-	public Date getDop() {
-		return Dop;
-	}
-	public void setDop(Date dop) {
-		Dop = dop;
-	}
-	public Product(String pname, double price, Date dop) {
-		super();
-		this.pname = pname;
-		this.price = price;
-		Dop = dop;
-	}
+    private long ID;
+    private String name;
+    private BigDecimal price;
+    private Date dateAdded;
+    
+    public Product() {
+            
+    }
+    public Product(long id, String name, BigDecimal price, Date dateAdded) {
+            this.ID = id;
+            this.name = name;
+            this.price = price;
+            this.dateAdded = dateAdded;
+    }
+    
+    public long getID() {return this.ID; }
+    public String getName() { return this.name;}
+    public BigDecimal getPrice() { return this.price;}
+    public Date getDateAdded() { return this.dateAdded;}
+    
+    public void setID(long id) { this.ID = id;}
+    public void setName(String name) { this.name = name;}
+    public void setPrice(BigDecimal price) { this.price = price;}
+    public void setDateAdded(Date date) { this.dateAdded = date;}
+    
+    
 }
+
